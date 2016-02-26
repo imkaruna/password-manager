5e619751558d42deb3335e221a604a39adac30ec
Karuna <imkaruna@gmail.com>
2016-02-25 23:52:49 -0500

    Modified Account to handle date password changed

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,7 +3,7 @@ class Account < ActiveRecord::Base
   belongs_to :user
   def password_expires
     todays_date = DateTime.now
-    binding.pry
+    #binding.pry
     expiry_date = self.password_changed_date
     return (expiry_date - todays_date).to_i
   end
